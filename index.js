993d82da0785bfcbdcc7ee093709ebefe088b7dd
Georgi Despotov <79579263+GeorgiDespotov@users.noreply.github.com>
2022-02-10 16:04:56 +0200

    exercise ts classes

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+exports.Melolemonmelon = exports.Earthmelon = exports.Airmelon = exports.Firemelon = exports.Watermelon = exports.Maniger = exports.Senior = exports.Junior = void 0;
 var User = /** @class */ (function () {
     function User(name) {
         this.name = name;
@@ -73,6 +91,150 @@ var info2 = new Tickets([
     'New York City|95.99|sold',
     'Philadelphia|94.20|available'
 ], 'price');
-console.log(info.outputh());
-console.log(info1.outputh());
-console.log(info2.outputh());
+// console.log(info.outputh());
+// console.log(info1.outputh());
+// console.log(info2.outputh());
+var Employee = /** @class */ (function () {
+    function Employee(name, age) {
+        this.name = name;
+        this.age = age;
+        this.salary = 0;
+        this.tasks = [];
+    }
+    Employee.prototype.work = function () {
+        var currentTask = this.tasks.shift();
+        this.tasks.push(currentTask);
+        console.log(this.name + currentTask);
+    };
+    Employee.prototype.getSalary = function () {
+        return this.salary;
+    };
+    Employee.prototype.collectSalary = function () {
+        console.log("".concat(this.name, " received ").concat(this.getSalary(), " this mounth."));
+    };
+    return Employee;
+}());
+var Junior = /** @class */ (function (_super) {
+    __extends(Junior, _super);
+    function Junior(name, age) {
+        var _this = _super.call(this, name, age) || this;
+        _this.tasks.push(' is working on a simple task.');
+        return _this;
+    }
+    return Junior;
+}(Employee));
+exports.Junior = Junior;
+var Senior = /** @class */ (function (_super) {
+    __extends(Senior, _super);
+    function Senior(name, age) {
+        var _this = _super.call(this, name, age) || this;
+        _this.tasks.push(' is working on a complicated task.');
+        _this.tasks.push(' is taking time off work.');
+        _this.tasks.push(' is supervising junior workers.');
+        return _this;
+    }
+    return Senior;
+}(Employee));
+exports.Senior = Senior;
+var Maniger = /** @class */ (function (_super) {
+    __extends(Maniger, _super);
+    function Maniger(name, age) {
+        var _this = _super.call(this, name, age) || this;
+        _this.divident = 0;
+        _this.tasks.push(' scheduled a meeting.');
+        _this.tasks.push(' is preparing a quarterly report.');
+        return _this;
+    }
+    Maniger.prototype.getSalary = function () {
+        return this.salary + this.divident;
+    };
+    return Maniger;
+}(Employee));
+exports.Maniger = Maniger;
+var maniger = new Maniger('Pesho', 34);
+maniger.salary = 200;
+maniger.divident = 50;
+// console.log(maniger.getSalary());
+// console.log(maniger.collectSalary());
+// console.log(maniger.work());
+// console.log(maniger.work());
+var Melon = /** @class */ (function () {
+    function Melon(weight, sort) {
+        this.weight = weight;
+        this.sort = sort;
+        this.element = '';
+    }
+    Melon.prototype.getElIndex = function () {
+        return this.weight * this.sort.length;
+    };
+    Melon.prototype.toString = function () {
+        console.log("Element: ".concat(this.element, "\nSort: ").concat(this.sort, "\nElement Index: ").concat(this.getElIndex()));
+    };
+    return Melon;
+}());
+var Watermelon = /** @class */ (function (_super) {
+    __extends(Watermelon, _super);
+    function Watermelon(weigth, sort) {
+        var _this = _super.call(this, weigth, sort) || this;
+        _this.element = 'Water';
+        return _this;
+    }
+    return Watermelon;
+}(Melon));
+exports.Watermelon = Watermelon;
+var Firemelon = /** @class */ (function (_super) {
+    __extends(Firemelon, _super);
+    function Firemelon(weigth, sort) {
+        var _this = _super.call(this, weigth, sort) || this;
+        _this.element = 'Fire';
+        return _this;
+    }
+    return Firemelon;
+}(Melon));
+exports.Firemelon = Firemelon;
+var Airmelon = /** @class */ (function (_super) {
+    __extends(Airmelon, _super);
+    function Airmelon(weigth, sort) {
+        var _this = _super.call(this, weigth, sort) || this;
+        _this.element = 'Air';
+        return _this;
+    }
+    return Airmelon;
+}(Melon));
+exports.Airmelon = Airmelon;
+var Earthmelon = /** @class */ (function (_super) {
+    __extends(Earthmelon, _super);
+    function Earthmelon(weigth, sort) {
+        var _this = _super.call(this, weigth, sort) || this;
+        _this.element = 'Earth';
+        return _this;
+    }
+    return Earthmelon;
+}(Melon));
+exports.Earthmelon = Earthmelon;
+var Melolemonmelon = /** @class */ (function (_super) {
+    __extends(Melolemonmelon, _super);
+    function Melolemonmelon(weigth, sort) {
+        var _this = _super.call(this, weigth, sort) || this;
+        _this.allElemets = ['Water', 'Fire', 'Earh', 'Air'];
+        return _this;
+    }
+    Melolemonmelon.prototype.morph = function () {
+        var currentElemnt = this.element = this.allElemets.shift();
+        this.allElemets.push(this.element);
+        return this.element;
+    };
+    Melolemonmelon.prototype.toString = function () {
+        console.log("Element: ".concat(this.morph(), "\nSort: ").concat(this.sort, "\nElement Index: ").concat(this.getElIndex()));
+    };
+    return Melolemonmelon;
+}(Earthmelon));
+exports.Melolemonmelon = Melolemonmelon;
+var wMelon = new Watermelon(2, 'on');
+console.log(wMelon.toString());
+var morphMelon = new Melolemonmelon(5, 'Pulp');
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
--- index.ts
+++ index.ts
@@ -108,9 +108,175 @@ let info2 = new Tickets([
     'Philadelphia|94.20|available'
 ],
     'price');
-console.log(info.outputh());
-console.log(info1.outputh());
-console.log(info2.outputh());
+// console.log(info.outputh());
+// console.log(info1.outputh());
+// console.log(info2.outputh());
+
+
+abstract class Employee {
+
+    public name: string;
+    public age: number;
+    public salary: number;
+    public tasks: Array<string | undefined>;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        this.salary = 0;
+        this.tasks = [];
+    }
+
+    public work(): void {
+        const currentTask = this.tasks.shift();
+        this.tasks.push(currentTask);
+        console.log(this.name + currentTask);
+    }
+
+    public getSalary() {
+        return this.salary;
+    }
+
+    public collectSalary(): void {
+        console.log(`${this.name} received ${this.getSalary()} this mounth.`);
+    }
+}
+
+export class Junior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(' is working on a simple task.');
+    }
+}
+
+export class Senior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(' is working on a complicated task.');
+        this.tasks.push(' is taking time off work.');
+        this.tasks.push(' is supervising junior workers.');
+    }
+}
+
+export class Maniger extends Employee {
+
+    public divident: number;
+
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.divident = 0;
+        this.tasks.push(' scheduled a meeting.');
+        this.tasks.push(' is preparing a quarterly report.');
+    }
+
+    public getSalary() {
+        return this.salary + this.divident;
+    }
+}
+
+
+
+const maniger = new Maniger('Pesho', 34);
+maniger.salary = 200;
+maniger.divident = 50;
+// console.log(maniger.getSalary());
+// console.log(maniger.collectSalary());
+// console.log(maniger.work());
+// console.log(maniger.work());
+
+
+
+abstract class Melon {
+
+    public weight: number;
+    public sort: string;
+    public element: string | undefined;
+
+    constructor(weight: number, sort: string) {
+        this.weight = weight;
+        this.sort = sort;
+        this.element = '';
+    }
+
+    public getElIndex() {
+        return this.weight * this.sort.length;
+    }
+
+    public toString(): void {
+        console.log(`Element: ${this.element}\nSort: ${this.sort}\nElement Index: ${this.getElIndex()}`);
+    }
+}
+
+export class Watermelon extends Melon {
+
+    constructor(weigth: number, sort: string) {
+        super(weigth, sort);
+        this.element = 'Water';
+    }
+}
+
+export class Firemelon extends Melon {
+
+    constructor(weigth: number, sort: string) {
+        super(weigth, sort);
+        this.element = 'Fire';
+    }
+}
+
+export class Airmelon extends Melon {
+
+    constructor(weigth: number, sort: string) {
+        super(weigth, sort);
+        this.element = 'Air';
+    }
+}
+
+export class Earthmelon extends Melon {
+
+    constructor(weigth: number, sort: string) {
+        super(weigth, sort);
+        this.element = 'Earth';
+    }
+}
+
+export class Melolemonmelon extends Earthmelon {
+
+    private allElemets: Array<string>;
+
+    constructor(weigth: number, sort: string) {
+        super(weigth, sort);
+        this.allElemets = ['Water', 'Fire', 'Earh', 'Air'];
+        
+    }
+
+    private morph() {
+        const currentElemnt = this.element = this.allElemets.shift();
+        this.allElemets.push(this.element!);
+
+        return this.element;
+    }
+
+    public toString(): void {
+        console.log(`Element: ${this.morph()}\nSort: ${this.sort}\nElement Index: ${this.getElIndex()}`);
+    }
+}
+
+let wMelon = new Watermelon(2, 'on');
+console.log(wMelon.toString());
+
+const morphMelon = new Melolemonmelon(5, 'Pulp');
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+console.log(morphMelon.toString());
+
+
+
+
+
+
+
 
 
 
